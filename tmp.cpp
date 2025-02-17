32a78a450a4882f0da4efc08e709de20c1d07fa6
0x3ea <1437412880@qq.com>
2025-02-17 20:47:04 +0800

    second commit

--- tmp.cpp
+++ tmp.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-namespace printhello
+class A;
+class B
 {
-    void print(std::string name)
-    {
-        std::cout << "Hello, " << name << std::endl;
-        // std::cout << "\033[1;41;32m"<< "Hello, " << name << std::endl;
-    }
-}
-// using namespace printhello;
+    void f(A a); 
+};
+class A
+{
+    void g(B b);
+};
 int main() {
 
     
-    // 设置粗体文本，红色背景，绿色前景
-    // std::cout << "\033[1;41;32m" << "这是粗体，红色背景，绿色前景的文本" << "\033[0m" << std::endl;
-    printhello::print(std::string("you"));
+   
     return 0;
 }
