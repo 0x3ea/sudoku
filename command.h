6b2b98bd00024dcf2319d119f80db72a3aca94ab
0x3ea <1437412880@qq.com>
2025-02-17 21:21:15 +0800

    third commit

--- command.h
+++ command.h
@@ -6,13 +6,32 @@
 #include "common.h"
 
 class CScene;
-class CCommand
+class CCommand//操作命令的类，可能涉及执行和撤销操作
 {
 
 public:
-    
+    //构造函数
+    CCommand(CScene* pOwner);
+    CCommand(CScene *pOwner, const point_t &point, int preValue, int curValue);
+    //拷贝构造函数
+    CCommand(const CCommand &);
+    //析构函数，通常用于释放资源（如内存、文件句柄等）
+    ~CCommand();
 
+    bool execute(int nInputValue);  //执行命令
+    void undo();  //撤销命令 
+    point_t getPoint() { return _stPoint; }  //表示该命令操作的数独格子的坐标位置
+    int getPreValue() { return _nPreValue; }  //表示该命令操作之前的数值
+    int getCurValue() { return _nCurValue; }  //表示该命令操作之后的数值
+    void setPoint(const point_t &point) { _stPoint = point; }  
+    void setPreValue(int preValue) { _nPreValue = preValue; }
+    void setCurValue(int curValue) { _nCurValue = curValue; }
 
-};
+private:
+    CScene* _pOwner;
+    point_t _stPoint;
+    int _nPreValue;
+    int _nCurValue;  // actually the member is never used
+};  
 
 #endif
--- tmp.cpp
+++ tmp.cpp
@@ -2,15 +2,23 @@
 class A;
 class B
 {
-    void f(A a); 
+   
+    public:
+    void fun()
+    {
+        // x->yabbaDabbaDo();//错误：A类的对象在定义之前被使用
+    }
+    private:
+        A *x; //正确，经过前向引用声明，可以声明A类的对象指针
 };
 class A
 {
-    void g(B b);
+    public:
+        void yabbaDabbaDo();
+    private:
+        B* y;
+
 };
 int main() {
-
-    
-   
     return 0;
 }
